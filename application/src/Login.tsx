65c6d88c0cd4e874491de6ec939acc295b214c2a
Jonathan Palumbo <jpalumbo@riskalyze.com>
2018-01-12 15:15:01 -0800

    Adding splash screen header.

--- application/src/Login.tsx
+++ application/src/Login.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import {ImageBackground, StatusBar, StyleSheet} from "react-native";
-import {Container} from "native-base"
+import {Dimensions, ImageBackground, StatusBar, StyleSheet, View} from "react-native";
+import {Container, H1} from "native-base"
 
+
+const deviceHeight = Dimensions.get("window").height;
 const splashScreenBackground = require("../assets/splash.jpg");
 
 export default class Login extends React.Component<{}> {
@@ -11,6 +13,12 @@ export default class Login extends React.Component<{}> {
                 <StatusBar barStyle="light-content" />
                 <ImageBackground source={splashScreenBackground} style={styles.container}>
                 </ImageBackground>
+                <View
+                    style={styles.headerContainer}>
+                    <View style={{marginTop: 20}} />
+                    <H1 style={styles.text}>First Pull</H1>
+                    <View style={{marginTop: 8}} />
+                </View>
             </Container>
         );
     }
@@ -22,6 +30,23 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    text: {
+        color: "#D8D8D8",
+        bottom: 6,
+        marginTop: 5
+    },
+    label: {
+        color: "#D8D8D8",
+    },
+    icon: {
+        color: "#D8D8D8",
+    },
+    headerContainer: {
+        alignItems: "center",
+        marginBottom: 50,
+        marginTop: deviceHeight / 8,
+        backgroundColor: "transparent"
     },
 
 });
